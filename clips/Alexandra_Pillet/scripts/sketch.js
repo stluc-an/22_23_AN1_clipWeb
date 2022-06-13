ae5c33576b40fe739c771148e1a7eeeffd6ce2f0
mememeshira <100421880+mememeshira@users.noreply.github.com>
2022-06-13 17:25:35 +0200

    21_22_AN1_clipWeb

--- clips/Alexandra_Pillet/scripts/sketch.js
+++ clips/Alexandra_Pillet/scripts/sketch.js
@@ -4,7 +4,8 @@
   @Date:   2021-02-04 22:38:59
   @Last Modified time: 2022-02-10 22:46:49
 \*----------------------------------------*/
-let audioPath = "./assets/audio/Sous Plis_Discret.mp3";
+
+let audioPath = "./assets/audio/sousplis2.mp3";
 let sequencer;
 
 function setup() {	
@@ -16,26 +17,88 @@ function setup() {
 	sequencer = new Sequencer(audioPath, 132);
 	
 	sequencer.registerSequence({
-		name : "all",
+        name : "intro",
+        start:1,
+        stop:10,
 		onStart : (event) => {
+            document.querySelector(".circleWrapper").classList.add("anim") 
+            circles = true;
 			console.log(event)
 		},
 		onStep : (event) => {
+            circles = true
 			console.log(event)
 		},
 		onStop : (event) => {
+            
+            document.querySelector(".circleWrapper").classList.remove("anim") 
+            circles = false;
 			console.log(event)
 		},
 		
 	});
-	}
-
-function draw(){
-	sequencer.update();
-	
+    
+    	
+	sequencer.registerSequence({
+        name : "second",
+        start:11,
+        stop:20,
+		onStart : (event) => {
+            document.querySelector(".circleWrapper2").classList.add("anim2") 
+            circles = true;
+			console.log(event)
+		},
+		onStep : (event) => {
+            circles = true
+			console.log(event)
+		},
+		onStop : (event) => {
+            
+            document.querySelector(".circleWrapper2").classList.remove("anim2") 
+            circles = false;
+			console.log(event)
+		},
+		
+	});     
 }
+ 
+function draw(){
+	sequencer.update()}
+
+var snail = function draw(){
+    globalResult = ();
+	let circles = [...document.querySelectorAll(".circle")];
+    let count = 0;
+    
+    let rayon = 0;
+    let angle = 0;//frameCount *0.1;
+    for(let circle of circles){
+
+        circle.style.animationDelay = noise(count, 0)* 500+ "ms";
+        
+        circle.style.left = width/2 + rayon * cos(angle) + "px";
+        circle.style.top = height/2 + rayon * sin(angle) + "px";
+        
+        rayon += 30;
+        angle += 0.8;
+        
+        };
+    printIn(globalResult);
+    /*let circles2 = [...document.querySelectorAll(".circle2")];
+    let count = 0;
+    for(let circle of circles){
+        circle.style.animationDelay = noise(count, 0)* 500+ "ms";
+        circle.style.left = noise(frameCount*0.01, count, 1)* width + 'px';
+        circle.style.top = noise(frameCount*0.01,count, 2)* height + 'px';
+         
+        }*/
+    count++;
+        
+}
+
+snail();
+
 
 function windowResized(){
 	resizeCanvas(window.innerWidth, window.innerHeight);
 }
-
