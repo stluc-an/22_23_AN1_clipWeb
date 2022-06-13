56316667990288e702939782635d7d7eb2daf1ae
vincent evrard <vinceevrardgpv@gmail.com>
2022-06-13 23:55:55 +0200

    starnge

--- exemples/animation/scripts/animator.js
+++ exemples/animation/scripts/animator.js
@@ -1,37 +1,31 @@
 
-
-function numberFormat(nbr, lenght){
-    nbr = nbr + ""; 
-    //chaine de caractère
-    nbr.length;
-    for(;nbr.length < lenght;){
-        nbr = "0" +nbr;
-    }
-    console.log(nbr);
-    return nbr;
+function numberFormat(nbr, length) {
+	nbr = nbr +"";
+	for(;nbr.length < length;){
+		nbr="0"+nbr;
+	}
+	console.log(nbr);
+	return nbr;
 }
 
-
-
 class Animator {
-    constructor(addressPatern,nbrImg){
-        this.images = [];
-        for(let compteur = 1; compteur <= nbrImg ; compteur ++){
-            let imgName = numberFormat(compteur,3);
-            let address = addressPatern.replace("_CHIFFRE_", imgName);
-            let img = loadImage(address);
-            this.images.push(img);
-            console.log(address);
-        }
-        
-        
-         //loadImage(addressPatern)
-        
-    }
-  draw(CURSOR, x, y, w, h){
-    let imgNumber = floor (CURSOR * this.images.length) ; 
-      let img = this.images[imgNumber];
-      image(img, x, y, w, h)  
-      
-  }  
+	constructor(addressPatern, nbrImg){
+		this.images = [];
+		this.imgNumber = 0;
+		for(let compteur = 0; compteur < nbrImg ; compteur++){
+			 let imgName = numberFormat(compteur, 1)
+			 let address= addressPatern.replace("_CHIFFRE_",imgName)
+			 let img = loadImage(address);
+			 this.images.push(img);
+		}
+	}
+	update(cursor){
+		this.imgNumber = cursor=floor(cursor * this.images.length);
+		
+	}
+	draw(cursor, x , y, w ,h){
+		let img =this.images[ this.imgNumber]
+		image(img, x, y, w, h)
+
+	}				
 }
