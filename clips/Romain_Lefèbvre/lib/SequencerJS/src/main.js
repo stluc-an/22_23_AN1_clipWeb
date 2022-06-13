5dabedd5a4971e5a7629efdf795a36925ffb9224
vincent evrard <vinceevrardgpv@gmail.com>
2022-06-13 23:54:05 +0200

    clean lib file

--- clips/Romain_Lefèbvre/lib/SequencerJS/src/main.js
+++
@@ -1,9 +0,0 @@
-/*----------------------------------------*\
-  21.2.camera - main.js
-  @author Evrard Vincent (vincent@ogre.be)
-  @Date:   2021-02-14 16:46:00
-  @Last Modified time: 2021-02-14 17:12:06
-\*----------------------------------------*/
-import Sequencer from "./Sequencer.js";
-
-window.Sequencer = Sequencer;
