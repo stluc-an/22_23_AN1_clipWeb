d4ba07a68a81b9422efc1e44356362bc95dc5d06
nelljst <120729080+nelljst@users.noreply.github.com>
2023-06-10 16:35:37 +0200

    mise en ligne de mon super projet(nell)

---
+++ clips/jeusette_nell/scripts/Drawable.js
@@ -0,0 +1,20 @@
+/*----------------------------------------*\
+  22_23_AN1_clipWeb - drawable.js
+  @author Evrard Vincent (vincent@ogre.be)
+  @Date:   2023-03-17 12:17:07
+  @Last Modified time: 2023-03-17 12:22:36
+\*----------------------------------------*/
+
+
+class Drawable{
+	constructor(){
+		this._drawable = false;
+	}
+	setDrawable(value){
+		this._drawable = value;
+	}
+	isDrawable(){
+		return this._drawable;
+	}
+}
+
